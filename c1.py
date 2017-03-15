e428171f367a60f609bd016dcf969a0f2b29548a
haoziiy <136842490@qq.com>
2017-03-15 12:41:14 +0800

    爬豆瓣电影

--- c1.py
+++ c1.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-content = requests.get('http://www.qiushibaike.com').content
+content = requests.get('https://movie.douban.com').content
 soup = BeautifulSoup(content, 'html.parser')
 
-for div in soup.find_all('div',{'class':'content'}):
+for div in soup.find_all('div',{'class':'review-content'}):
     print div.text.strip()
