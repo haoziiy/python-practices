2ddc4188fd4d45fe9db81a5236e815cc15662d85
haoziiy <joker.xieyuri@qq.com>
2017-04-04 21:57:15 +0800

    爬虫实战：爬取百度百科1000个页面信息

--- baike_spider/html_downloader.py
+++ baike_spider/html_downloader.py
@@ -1,5 +1,11 @@
+import urllib2
+
+
 class HtmlDownloader():
-    def download(self, new_url):pass
-
-
-    pass
+    def download(self, url):
+        if url is None:
+            return None
+        response = urllib2.urlopen(url)
+        if response.getcode() != 200:
+            return None
+        return response.read()
--- baike_spider/html_outputer.py
+++ baike_spider/html_outputer.py
@@ -1,4 +1,27 @@
 class HtmlOutputer():
-    def collect_data(self, new_data):pass
-    def output_html(self):pass
-    pass
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if data == None:
+            return
+        self.datas.append(data)
+
+
+    def output_html(self):
+        fout = open('output.html', 'w')
+
+        fout.write('<html>')
+        fout.write('<body>')
+        fout.write('<table>')
+
+        for data in self.datas:
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['url'])
+            fout.write('</tr>')
+
+        fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
+
+        fout.close()
